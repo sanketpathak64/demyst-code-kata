aa5dbdbee85344d97fee2ca205486d3186de71f2
sanketpathak64 <sanketpathak64@gmail.com>
2024-04-26 16:57:21 +0530

    Added code for fetching and printing todos

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("hello")
+from dotenv import load_dotenv
+import os
+from integerations.api import get_todo
+load_dotenv()
+
+def get_todos():
+    even_todo_limit = int(os.environ.get("TODO_LIMIT"))
+    todos = [get_todo(i) for i in range(2, 2 * even_todo_limit + 1, 2)]
+    return todos
+
+def display_todos(todos):
+    for todo in todos:
+        print(todo['title'], " ", todo['title'], " ", todo['completed'])
+
+display_todos(get_todos())
